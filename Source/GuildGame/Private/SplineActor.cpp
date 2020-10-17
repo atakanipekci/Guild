781300343d33af435b1207c93e58eda5456cec26
Atakan İpekçi <ataipekci@gmail.com>
2020-10-17 15:24:01 +0300

    *fix - restored the files that were somehow deleted by git

---
+++ Source/GuildGame/Private/SplineActor.cpp
@@ -0,0 +1,191 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SplineActor.h"
+
+#include "GGLogHelper.h"
+
+// Sets default values
+ASplineActor::ASplineActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	SplineComponent = CreateDefaultSubobject<USplineComponent>("Spline");
+	if(SplineComponent)
+	{
+		SetRootComponent(SplineComponent);  
+	}
+
+	LOG("aspline created");
+}
+
+void ASplineActor::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+	ClearNodes();
+	for(int i = 0; i < NodeCount; i++)
+	{
+		AddNode(FVector{static_cast<float>(i*200),0,0});
+	}
+	UpdateSpline();
+}
+
+void ASplineActor::AddNode(FVector Position)
+{
+	if(SplineComponent == nullptr)
+	{
+		return;
+	}
+
+	SplineComponent->AddSplineWorldPoint(Position);
+}
+
+void ASplineActor::ClearNodes()
+{
+	if(SplineComponent == nullptr)
+	{
+		return;
+	}
+
+	SplineComponent->ClearSplinePoints(true);
+}
+
+void ASplineActor::UpdateSpline()
+{
+	if(SplineComponent == nullptr)
+	{
+		return;
+	}
+	//ClearNodes();
+	LOG("%d components destroyed", SplineComponent->GetNumChildrenComponents());
+	for(int i = SplineComponent->GetNumChildrenComponents()-1; i >= 0; i--)
+	{
+		USceneComponent* comp = SplineComponent->GetChildComponent(i);
+		//comp->DetachFromComponent(FDetachmentTransformRules::KeepWorldTransform);
+		//comp->UnregisterComponent();
+		comp->DestroyComponent();
+		//SplineComponent->GetChildComponent(i)->DestroyComponent();
+	}
+	if(SplineComponent && SplineMeshMap.Num() > 0)
+	{
+		// lookup all pertinent values
+		FSplineMeshDetails* StartMeshDetails = nullptr;
+		if(SplineMeshMap.Contains(ESplineMeshType::START))
+		{
+			StartMeshDetails = SplineMeshMap.Find(ESplineMeshType::START);  
+		}
+		
+		FSplineMeshDetails* EndMeshDetails = nullptr;
+		if(SplineMeshMap.Contains(ESplineMeshType::END))
+		{
+			EndMeshDetails = SplineMeshMap.Find(ESplineMeshType::END);
+		}
+
+		FSplineMeshDetails* DefaultMeshDetails = nullptr;
+		if(SplineMeshMap.Contains(ESplineMeshType::DEFAULT))
+		{
+			DefaultMeshDetails = SplineMeshMap.Find(ESplineMeshType::DEFAULT);  
+		}
+		else
+		{
+			// exit if we don't have a default mesh to work with
+			return;
+		}
+
+		const int32 SplinePoints = SplineComponent->GetNumberOfSplinePoints();
+
+
+		LOG("%d components added", SplinePoints - 1 );
+		int ffs = 0;
+		for(int SplineCount = 0; SplineCount < (SplinePoints - 1); SplineCount++)
+		{
+
+			USplineMeshComponent *SplineMesh = NewObject<USplineMeshComponent>(this, USplineMeshComponent::StaticClass());
+			UStaticMesh* StaticMesh = DefaultMeshDetails->Mesh;
+			UMaterialInterface* Material = nullptr;
+			ESplineMeshAxis::Type ForwardAxis = DefaultMeshDetails->ForwardAxis;
+			// start mesh
+			if(StartMeshDetails && StartMeshDetails->Mesh && SplineCount == 0)
+			{
+				StaticMesh = StartMeshDetails->Mesh;
+				ForwardAxis = StartMeshDetails->ForwardAxis;
+
+				if(StartMeshDetails->DefaultMaterial)
+				{
+				  Material = StartMeshDetails->DefaultMaterial;
+				}          
+			}
+			else if(EndMeshDetails && EndMeshDetails->Mesh && SplinePoints > 2 && SplineCount == (SplinePoints - 2))
+			{
+				// end mesh
+				StaticMesh = EndMeshDetails->Mesh;
+				ForwardAxis = EndMeshDetails->ForwardAxis;
+
+				if(EndMeshDetails->DefaultMaterial)
+				{
+				  Material = EndMeshDetails->DefaultMaterial;
+				}  
+			}
+			else
+			{
+				// default assignment - middle mesh
+				if(DefaultMeshDetails->AlternativeMaterial && SplineCount > 0 && SplineCount % 2 == 0)
+				{
+				  Material = DefaultMeshDetails->AlternativeMaterial;
+				}
+				else if(DefaultMeshDetails->DefaultMaterial)
+				{
+				  Material = DefaultMeshDetails->DefaultMaterial;
+				}
+			}
+
+			// update mesh details
+			SplineMesh->SetStaticMesh(StaticMesh);
+			SplineMesh->SetForwardAxis(ForwardAxis, true);
+			SplineMesh->SetMaterial(0, Material);
+
+
+			// initialize the object
+			SplineMesh->RegisterComponent();
+			//SplineMesh->RegisterComponentWithWorld(GetWorld());
+
+			SplineMesh->CreationMethod = EComponentCreationMethod::UserConstructionScript;
+			SplineMesh->SetMobility(EComponentMobility::Movable);
+
+			bool ffsbool = SplineMesh->AttachToComponent(SplineComponent, FAttachmentTransformRules::KeepRelativeTransform);
+			if(ffsbool)
+			{
+				ffs++;
+			}
+			
+			LOG("%d total child components", SplineComponent->GetNumChildrenComponents());
+			// define the positions of the points and tangents
+			const FVector StartPoint = SplineComponent->GetLocationAtSplinePoint(SplineCount, ESplineCoordinateSpace::Type::Local);
+			const FVector StartTangent = SplineComponent->GetTangentAtSplinePoint(SplineCount, ESplineCoordinateSpace::Type::Local);
+			const FVector EndPoint = SplineComponent->GetLocationAtSplinePoint(SplineCount + 1, ESplineCoordinateSpace::Type::Local);
+			const FVector EndTangent = SplineComponent->GetTangentAtSplinePoint(SplineCount + 1, ESplineCoordinateSpace::Type::Local);
+			SplineMesh->SetStartAndEnd(StartPoint, StartTangent, EndPoint, EndTangent, true);
+
+			// query physics
+			SplineMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		}
+		LOG("%d reee components added", ffs );
+		
+	}
+	SplineComponent->UpdateSpline();
+}
+
+// Called when the game starts or when spawned
+void ASplineActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASplineActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/GuildGame/Public/SplineActor.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameFramework/Actor.h"
+#include "Components/SplineComponent.h"
+#include "Components/SplineMeshComponent.h"
+#include "SplineActor.generated.h"
+
+UENUM(BlueprintType)
+enum class ESplineMeshType: uint8 {
+  DEFAULT    UMETA(DisplayName = "Default Mesh"),
+  START    UMETA(DisplayName = "Starting Mesh"),
+  END      UMETA(DisplayName = "EndingMesh"),
+};
+USTRUCT(BlueprintType)
+struct FSplineMeshDetails : public FTableRowBase
+{
+  GENERATED_BODY()
+  
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  UStaticMesh* Mesh;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly)
+  TEnumAsByte<ESplineMeshAxis::Type> ForwardAxis;
+  
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+  class UMaterialInterface* DefaultMaterial;
+  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+  class UMaterialInterface* AlternativeMaterial;
+  FSplineMeshDetails() : ForwardAxis(ESplineMeshAxis::Type::X)
+  {
+  }
+};
+
+UCLASS()
+class GUILDGAME_API ASplineActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASplineActor();
+	void OnConstruction(const FTransform& Transform) override;
+	void AddNode(FVector Position);
+	void ClearNodes();
+	void UpdateSpline();
+	UPROPERTY(VisibleAnywhere, Category = "Spline")
+	USplineComponent* SplineComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Spline")
+	TMap<ESplineMeshType, FSplineMeshDetails> SplineMeshMap;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Spline")
+	int NodeCount = 3;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

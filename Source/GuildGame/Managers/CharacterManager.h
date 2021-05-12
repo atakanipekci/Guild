c914d7786bdf5b3fdea241e7ef1907734db34f2d
Atakan <ataipekci@gmail.com>
2021-05-12 18:37:06 +0300

    *new - Character Spawn function
    -Implemented a generic function to spawn characters with a given class
    -Changed old codes to use the new function

--- Source/GuildGame/Managers/CharacterManager.h
+++ Source/GuildGame/Managers/CharacterManager.h
@@ -3,6 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GuildGameInstance.h"
+#include "GuildGame/Town/Navigation/TownNpcManager.h"
+#include "Kismet/GameplayStatics.h"
+
+enum class ECharacterClassType: uint8;
 
 /**
  * 
@@ -14,4 +19,53 @@ public:
     static void SetDamageableGrids(class AGGCharacter* Character);
     static bool CanAttackTo(const class AGGCharacter*, const class AGGCharacter* );
     static class GridManager* CharGridManager;
+
+	template<typename T, typename E>
+	static T* SpawnCharacter(TSubclassOf<E> BPClass, ECharacterClassType CharacterType, FVector Location, FRotator Rotation, UWorld* World);
+
+	static FString GetCharFileRowName(ECharacterClassType CharacterType);
+
+	
 };
+
+template <typename T, typename E>
+T* CharacterManager::SpawnCharacter(TSubclassOf<E> BPClass, ECharacterClassType CharacterType, FVector Location,
+	FRotator Rotation, UWorld* World)
+{
+	if(World == nullptr)
+    {
+        return nullptr;
+    }
+    UGuildGameInstance* GameInstance = Cast<UGuildGameInstance>(UGameplayStatics::GetGameInstance(World));
+
+    if(GameInstance == nullptr || GameInstance->CharacterFileTable == nullptr)
+    {
+        return nullptr;
+    }
+    T* SpawnedCharacter =  World->SpawnActor<T>(BPClass, Location, Rotation);
+    if(SpawnedCharacter)
+    {
+        USkeletalMeshComponent* SkeletalMesh = Cast<USkeletalMeshComponent>(SpawnedCharacter->GetComponentByClass(USkeletalMeshComponent::StaticClass()));
+            
+        if(SkeletalMesh)
+        {
+            const FName RowName = *(GetCharFileRowName(CharacterType));
+            FCharFileDataTable* CharFiles = GameInstance->CharacterFileTable->FindRow<FCharFileDataTable>(RowName, "Character File Row Missing", true);
+
+            if(CharFiles)
+            {
+                if(CharFiles->SkeletalMesh)
+                {
+                    SkeletalMesh->SetSkeletalMesh(CharFiles->SkeletalMesh);
+        
+                    if(CharFiles->AnimationBP)
+                    {
+                        SkeletalMesh->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+                        SkeletalMesh->SetAnimInstanceClass(CharFiles->AnimationBP);
+                    }
+                }
+            }       
+        }
+    }
+    return SpawnedCharacter;
+}
